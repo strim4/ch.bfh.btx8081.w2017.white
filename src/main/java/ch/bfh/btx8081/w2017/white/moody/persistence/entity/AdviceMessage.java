48e2e000c524b15611002a6d456b644ab1704475
zbinc2 <32926166+zbinc2@users.noreply.github.com>
2017-11-30 13:46:27 +0100

    @Roberto, nur kleine Klammern korrigiert

--- src/main/java/ch/bfh/btx8081/w2017/white/moody/persistence/entity/AdviceMessage.java
+++ src/main/java/ch/bfh/btx8081/w2017/white/moody/persistence/entity/AdviceMessage.java
@@ -1,32 +1,31 @@
 package ch.bfh.btx8081.w2017.white.moody.persistence.entity;
 
 /**
- * @author Roberto
- * Advice messages
- * Last edit: 28.11.17
+ * @author Roberto Advice messages Last edit: 28.11.17
  */
 
 public class AdviceMessage {
-	
-	private String advice;
-	private int advideId;
-	
-	public AdviceMessage() {
-		
-		public String getAdvice() {
-			return advice;
-		}
-		
-		public int getAdviceId() {
-			return adviceId;
-		}
 
-		public void setAdvice(String advice) {
-			this.advice = advice;
-		}
-		
-		public void setAdviceId(int adviceId) {
-			this.adviceId = adviceId;
-		}
+	private String advice;
+	private int adviceId;
+
+	public AdviceMessage() {
+	}
+
+	public String getAdvice() {
+		return advice;
+	}
+
+	public int getAdviceId() {
+		return adviceId;
+	}
+
+	public void setAdvice(String advice) {
+		this.advice = advice;
+	}
+
+	public void setAdviceId(int adviceId) {
+		this.adviceId = adviceId;
+	}
 
 }
--- src/main/java/ch/bfh/btx8081/w2017/white/moody/persistence/entity/Profile.java
+++ src/main/java/ch/bfh/btx8081/w2017/white/moody/persistence/entity/Profile.java
@@ -1,101 +1,93 @@
 package ch.bfh.btx8081.w2017.white.moody.persistence.entity;
 
+import java.util.Date;
+
 /**
- * @author Roberto
- * Profile (including name, contact details)
- * Last edit: 28.11.17
+ * @author Roberto Profile (including name, contact details) Last edit: 28.11.17
  */
 
 public class Profile {
 	private int id;
 	private String firstName;
 	private String lastName;
-	private DateTime birthdate;
+	private Date birthdate;
 	private char gender;
 	private String address;
 	private String houseDoctor;
 	// private String desease; bin nicht sicher ob das auch gebraucht wird.
-	//private String therapy; bin nicht sicher ob das auch gebraucht wird.
-	
+	// private String therapy; bin nicht sicher ob das auch gebraucht wird.
+
 	public Profile() {
-	
+	}
+
 	public int getId() {
 		return id;
 	}
-		
+
 	public String getFirstName() {
 		return firstName;
 	}
-	
+
 	public String getLastName() {
 		return lastName;
 	}
-	
-	public DateTime getBirthdate() {
+
+	public Date getBirthdate() {
 		return birthdate;
 	}
-	
+
 	public char getGender() {
 		return gender;
 	}
-	
+
 	public String getAddress() {
 		return address;
 	}
-	
+
 	public String getHouseDoctor() {
 		return houseDoctor;
 	}
 	/*
-	 * public String getDesease() {
-	 * return desease;
-	 * }
-	 */
-	
-	/*
-	 * public String getTherapy() {
-	 * return therapy;
-	 * }
-	*/ 
-	
-	public void setId(int id) {
-		this.id = id;
-	}
-	
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	
-	public void setBirthdate(DateTime birthdate) {
-		this.birthdate = birthdate;
-	}
-	
-	public void setGender(char gender) {
-		this.gender = gender;
-	}
-	
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	
-	public void setHouseDoctor(String houseDoctor) {
-		this.houseDoctor = houseDoctor;
-	}
-	
-	/*
-	 * public void setDesease(String desease) {
-	 * this.desease = desease;
-	 * }
+	 * public String getDesease() { return desease; }
 	 */
 
 	/*
-	 * public void setTherapy(String therapy) {
-	 * this.therapy = therapy;
-	 * }
+	 * public String getTherapy() { return therapy; }
 	 */
+
+	public void setId(int id) {
+		this.id = id;
 	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setBirthdate(Date birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public void setHouseDoctor(String houseDoctor) {
+		this.houseDoctor = houseDoctor;
+	}
+
+	/*
+	 * public void setDesease(String desease) { this.desease = desease; }
+	 */
+
+	/*
+	 * public void setTherapy(String therapy) { this.therapy = therapy; }
+	 */
 }
