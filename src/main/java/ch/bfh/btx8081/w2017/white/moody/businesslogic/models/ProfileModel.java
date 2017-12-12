dbabc31c9c2e907c0b734d272f6fecc67ab6859e
zbinc2 <32926166+zbinc2@users.noreply.github.com>
2017-12-13 00:32:57 +0100

    some work on the profile

--- src/main/java/ch/bfh/btx8081/w2017/white/moody/businesslogic/models/ProfileModel.java
+++ src/main/java/ch/bfh/btx8081/w2017/white/moody/businesslogic/models/ProfileModel.java
@@ -3,11 +3,29 @@ package ch.bfh.btx8081.w2017.white.moody.businesslogic.models;
 import ch.bfh.btx8081.w2017.white.moody.persistence.entity.Profile;
 
 /**
+ * @author Chantal Zbinden
+ * Last Edit: 12.12.17
  * @author Roberto Tangarife 
  * Model for profile Stores the profile including name and contact details. 
  * Last Edit: 29.11.17
  */
 
 public class ProfileModel extends BaseModel {
+	
+	private Profile profile;
+	
+	public ProfileModel() {
+		
+		profile = new Profile();
+		
+	}
+	
+	public Profile getProfile() {
+		return profile;
+	}
+	
+	public void setProfile(Profile profile) {
+		this.profile = profile;
+	}
 
 }
