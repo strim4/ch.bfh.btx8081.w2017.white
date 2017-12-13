9d95214c093cffcb15a78f5451988d978608c1b0
zbinc2 <32926166+zbinc2@users.noreply.github.com>
2017-12-13 01:25:02 +0100

    add other views on the buttons from the start view

--- src/main/java/ch/bfh/btx8081/w2017/white/moody/presentation/presenter/BasePresenter.java
+++ src/main/java/ch/bfh/btx8081/w2017/white/moody/presentation/presenter/BasePresenter.java
@@ -10,7 +10,7 @@ import ch.bfh.btx8081.w2017.white.moody.presentation.views.Views;
 /**
  * Base Presenter Class
  * @author Chantal
- * Last edit: 30.11.17
+ * Last edit: 10.12.17
  *
  */
 @SuppressWarnings("serial")
@@ -35,22 +35,19 @@ public class BasePresenter implements ViewListener{
 			view.getUI().getNavigator().navigateTo(Views.DIARY_VIEW);
 			break;
 		case "buttonContact":
-			//view.getUI().getNavigator().navigateTo(Views.);
+			view.getUI().getNavigator().navigateTo(Views.CONTANCT_VIEW);
 			break;
 		case "buttonBarometer":
 			view.getUI().getNavigator().navigateTo(Views.BAROMETER_VIEW);
 			break;
 		case "buttonExercises":
-			//view.getUI().getNavigator().navigateTo(Views.);
+			view.getUI().getNavigator().navigateTo(Views.EXERCISES_VIEW);
 			break;
 		case "buttonInformations":
-			//view.getUI().getNavigator().navigateTo(Views.);
+			view.getUI().getNavigator().navigateTo(Views.INFORMATIONS_VIEW);
 			break;
 		case "buttonTips":
-			//view.getUI().getNavigator().navigateTo(Views.);
-			break;
-		case "buttonSettings":
-			//view.getUI().getNavigator().navigateTo(Views.);
+			view.getUI().getNavigator().navigateTo(Views.TIPS_VIEW);
 			break;
 			
 		}
