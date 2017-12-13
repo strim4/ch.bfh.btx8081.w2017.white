324836a2d9c444ab078642751891c847d778454f
zbinc2 <32926166+zbinc2@users.noreply.github.com>
2017-12-13 01:36:57 +0100

    deleting the deleteButton, add notification

--- src/main/java/ch/bfh/btx8081/w2017/white/moody/presentation/presenter/DiaryTextPresenter.java
+++ src/main/java/ch/bfh/btx8081/w2017/white/moody/presentation/presenter/DiaryTextPresenter.java
@@ -2,8 +2,8 @@ package ch.bfh.btx8081.w2017.white.moody.presentation.presenter;
 
 import java.util.Date;
 
-
-
+import com.vaadin.shared.Position;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.Button.ClickEvent;
 
 import ch.bfh.btx8081.w2017.white.moody.persistence.entity.DiaryText;
@@ -15,18 +15,15 @@ import ch.bfh.btx8081.w2017.white.moody.presentation.views.Views;
 /**
  * DiaryTextPresenter class
  * @author Chantal
- * Last edit: 30.11.17
+ * Last edit: 10.12.17
  */
 
 @SuppressWarnings("serial")
 public class DiaryTextPresenter implements ViewListener{
-	
-	
 
 	@SuppressWarnings("unused")
 	private DiaryText model;
 	private DiaryTextView view;
-	
 	
 	public DiaryTextPresenter(DiaryText model, DiaryTextView view) {
 		this.model = model;
@@ -34,23 +31,22 @@ public class DiaryTextPresenter implements ViewListener{
 		view.addListener(this);
 	}
 	
+	@SuppressWarnings("static-access")
 	public void buttonClick(ClickEvent event) {
 		
 		String buttonID = event.getButton().getId();
 		
 		switch(buttonID) {
-		case "buttonSave":
-			
+		case "buttonSave":			
 			DiaryText dt = new DiaryText(view.getNameValue(), view.getTFValue(), view.getDateValue(), new Date());
-			DBManager dbm = new DBManager();
-		dbm.persistObject(dt);
-			
+			DBManager dbm = new DBManager();		
+			dbm.persistObject(dt);
+			Notification saved = new Notification("");
+			saved.setPosition(Position.BOTTOM_CENTER);
+			saved.show("Eintrag gespeichert");
 			break;
 		case "buttonBack":
 			view.getUI().getNavigator().navigateTo(Views.DIARY_VIEW);
-			break;
-		case "buttonDelete":
-			//Lösch-Methode
 			break;
 		}
 	}
