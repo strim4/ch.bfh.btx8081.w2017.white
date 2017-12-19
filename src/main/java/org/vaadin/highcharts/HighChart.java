1a8aa36bb20c375be4f1e46c583da2b0d9f5f283
Sandra Burri <wohlfuehlquelle@gmail.com>
2017-12-19 21:38:04 +0100

    Neues Package erstellt für die Statistik

---
+++ src/main/java/org/vaadin/highcharts/HighChart.java
@@ -0,0 +1,8 @@
+package org.vaadin.highcharts;
+
+import com.vaadin.annotations.JavaScript;
+
+@JavaScript({"jquery-min.js", "highcharts.js", "highcharts-connector.js"})
+public class HighChart extends AbstractHighChart {
+ private static final long serialVersionUID = -7326315426217377753L;
+}
