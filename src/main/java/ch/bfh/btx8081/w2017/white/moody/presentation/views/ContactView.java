1b685905e74d824c639b450d2ac11782013baf4b
Roberto <edisonroberto.tangarifebonilla@students.bfh.ch>
2018-01-17 14:55:51 +0100

    ContactView modified

--- src/main/java/ch/bfh/btx8081/w2017/white/moody/presentation/views/ContactView.java
+++ src/main/java/ch/bfh/btx8081/w2017/white/moody/presentation/views/ContactView.java
@@ -1,11 +1,19 @@
 package ch.bfh.btx8081.w2017.white.moody.presentation.views;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.vaadin.server.FileResource;
 import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Button.ClickEvent;
+
+import ch.bfh.btx8081.w2017.white.moody.persistence.repository.implementation.DBManager;
 
 /**
  * ContactView class gives the contact data from the profile back
@@ -15,6 +23,8 @@ import com.vaadin.ui.Button.ClickEvent;
 @SuppressWarnings("serial")
 public class ContactView extends BaseView implements MoodyView {
 	
+	private static final String BUTTON_HEIGHT = null;
+	private static final String BUTTON_WIDTH = null;
 	private List<ViewListener> listeners = new ArrayList<ViewListener>();
 
 	public ContactView() {		
@@ -28,10 +38,45 @@ public class ContactView extends BaseView implements MoodyView {
 	 */
 	private void createFutureContent() {
 		
-		Label future = new Label("Auf dieser Seite kommen bald die Kontaktdaten fuer Notfaelle;)");
-		super.content.addComponent(future);
-		super.content.setComponentAlignment(future, Alignment.MIDDLE_CENTER);
+		String firstname = DBManager.getInstance().getFirstName();
+
+		Label labelArzt = new Label("Arzt");		
+		labelArzt.setId("labelArzt");
+		labelArzt.setWidth("190px");
+		labelArzt.setHeight("40px");
+
+		Label labelBezugsperson = new Label("Bezugsperson");		
+		labelBezugsperson.setId("labelBezugsperson");
+		labelBezugsperson.setWidth("190px");
+		labelBezugsperson.setHeight("40px");
 		
+		HorizontalLayout contentRow1 = new HorizontalLayout();
+		contentRow1.addComponents(labelArzt, labelBezugsperson);
+		content.addComponent(contentRow1);
+		content.setComponentAlignment(contentRow1, Alignment.MIDDLE_CENTER);
+		
+		Button buttonTelefonArzt = new Button("Tel");
+		buttonTelefonArzt.addClickListener(this);
+		buttonTelefonArzt.setId("buttonTelefonArzt");
+		buttonTelefonArzt.setWidth("190px");
+		buttonTelefonArzt.setHeight(BUTTON_HEIGHT);
+		
+		Button buttonTelefonBezugsperson = new Button("Tel");
+		buttonTelefonBezugsperson.addClickListener(this);
+		buttonTelefonBezugsperson.setId("buttonTelefonBezugsperson");
+		buttonTelefonBezugsperson.setWidth("190px");
+		buttonTelefonBezugsperson.setHeight(BUTTON_HEIGHT);
+		
+		HorizontalLayout contentRow2 = new HorizontalLayout();
+		contentRow2.addComponents(buttonTelefonArzt, buttonTelefonBezugsperson);
+		content.addComponent(contentRow2);
+		content.setComponentAlignment(contentRow2, Alignment.MIDDLE_CENTER);
+		
+		
+		//Label future = new Label("Auf dieser Seite kommen bald die Kontaktdaten fuer Notfaelle;)");
+		//super.content.addComponent(future);
+		//super.content.setComponentAlignment(future, Alignment.MIDDLE_CENTER);
+				
 		//TODO Kontaktdaten vom Profil
 	}
 	
