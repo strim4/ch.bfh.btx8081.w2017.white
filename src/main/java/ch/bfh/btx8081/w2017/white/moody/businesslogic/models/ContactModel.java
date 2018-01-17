b5befd40904ea0bdd8547457198b207e2a2301cd
Roberto <edisonroberto.tangarifebonilla@students.bfh.ch>
2018-01-17 14:54:17 +0100

    ContactModel

---
+++ src/main/java/ch/bfh/btx8081/w2017/white/moody/businesslogic/models/ContactModel.java
@@ -0,0 +1,19 @@
+package ch.bfh.btx8081.w2017.white.moody.businesslogic.models;
+
+public class ContactModel extends BaseModel {
+
+private ContactModel contactModel;
+	
+	public ContactModel() {
+		
+		/**contactModel = new ContactModel();
+	}
+	
+	public ContactModel getContactModel() {
+		return contactModel;
+	}
+	
+	public void setContactModel(ContactModel contactModel) {
+		this.contactModel = contactModel; */
+	}
+}
