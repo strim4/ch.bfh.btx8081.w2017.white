a5bd508e0782cc815f89887de6481f74db4335ff
zbinc2 <32926166+zbinc2@users.noreply.github.com>
2018-01-18 10:51:05 +0100

    add some text on the tips view

--- src/main/java/ch/bfh/btx8081/w2017/white/moody/presentation/views/TipsView.java
+++ src/main/java/ch/bfh/btx8081/w2017/white/moody/presentation/views/TipsView.java
@@ -3,6 +3,7 @@ package ch.bfh.btx8081.w2017.white.moody.presentation.views;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.vaadin.ui.Accordion;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Button.ClickEvent;
@@ -16,6 +17,12 @@ import com.vaadin.ui.Button.ClickEvent;
 public class TipsView extends BaseView implements MoodyView {
 
 	private List<ViewListener> listeners = new ArrayList<ViewListener>();
+	
+	private Accordion tips;
+	
+	private Label tip1;
+	private Label tip2;
+	private Label tip3;
 
 	public TipsView() {
 		
@@ -29,9 +36,34 @@ public class TipsView extends BaseView implements MoodyView {
 	 */
 	private void createFutureContent() {
 		
-		Label future = new Label("Auf dieser Seite kommen bald nuetzliche Tipps ;)");
-		super.content.addComponent(future);
-		super.content.setComponentAlignment(future, Alignment.MIDDLE_CENTER);
+		tips = new Accordion();
+		tips.setHeight("250px");
+		
+		tip1 = new Label("Tägliche Bewegung ist für einen gesunden Körper es­sen­zi­ell. "
+				+ "Sport kann auf depressive Personen lang anhaltende positive Effekte "
+				+ "haben, da der Körper dabei Endorphine (natürliche, antidepressiv "
+				+ "wirkende Hormone) ausschüttet. Diese fördern positive Gedanken und "
+				+ "können Glücksgefühle hervorrufen. Ideal wäre es, dreimal wöchentlich "
+				+ "für mindestens 20 Minuten zu trainieren – egal ob Joggen, Gewichte "
+				+ "heben, Walken, Fahrrad fahren oder Yoga.");
+		tip1.setWidth("1000px");
+		tips.addTab(tip1, "Tipp 1: Bewegung");
+		
+		tip2 = new Label("Eine Handvoll Kürbiskerne am Tag kann Ängstlichkeit und "
+				+ "depressive Gefühle mildern. Kürbiskerne enthalten L-Tryptophan, "
+				+ "welches beruhigend wirkt und den Blutdruck senkt.");
+		tip2.setWidth("1000px");
+		tips.addTab(tip2, "Tipp 2: Kürbiskerne");
+		
+		tip3 = new Label("Trink einen bis zwei Kaffee, um Depressionen zu verringern – "
+				+ "Koffein hebt die Stimmung. Trink aber nicht mehr als zwei Tassen täglich "
+				+ "(ansonsten kann es zum gegenteiligen Effekt kommen, also zu einer "
+				+ "Verstärkung der depressiven Stimmung).");
+		tip3.setWidth("1000px");
+		tips.addTab(tip3, "Tipp 3: Kaffee");
+		
+		super.content.addComponent(tips);
+		super.content.setComponentAlignment(tips, Alignment.MIDDLE_CENTER);
 		
 	}
 	
